41c3cea6f35e83bc4a372babe7ffc9d6418d3abc
DanieliniCaporal <dasacaporal@gmail.com>
2026-05-04 23:55:53 -0600

    feat(hooks): add initial hooks application structure

---
+++ 04-hooks-app/src/main.tsx
@@ -0,0 +1,15 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import { TrafficLightWithEffect } from './02-useEffect/TrafficLightWithEffect'
+// import { TrafficLight } from './01-useState/TrafficLight'
+
+
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    {/* <HooksApp /> */}
+    {/* <TrafficLight /> */}
+    <TrafficLightWithEffect />
+  </StrictMode>,
+)
